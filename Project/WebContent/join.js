acc6491508531bfda380aa7677c94e252714edc3
Gwangsu Kim <s138287@gmail.com>
2019-04-03 16:53:22 +0900

    commit

---
+++ Project/WebContent/join.js
@@ -0,0 +1,55 @@
+$(function() {
+	$("#register").click(function(e) {
+		if($("#input_id").val()==""){
+			alert('이름을 입력하세요');
+            $("#inputName").focus();
+            return false;
+		}
+		var email = $('#Input_Email').val();
+        if(email == ''){
+            alert('이메일을 입력하세요');
+            $("#Input_Email").focus();
+            return false;
+        }
+        
+        if($("#input_EmailCheck").val() ==''){
+            alert('이메일을 다시 한번 더 입력하세요');
+            $("#inputPasswordCheck").focus();
+            return false;
+        }
+        
+        if($("#input_Email").val()!== $("#input_EmailCheck").val()){
+            alert('이메일을 둘다 동일하게 입력하세요');
+            return false;
+        }
+
+        if($("#input_Password").val() ==''){
+            alert('비밀번호를 입력하세요');
+            $("#inputPassword").focus();
+            return false;
+        }
+
+        if($("#input_PasswordCheck").val() ==''){
+            alert('비밀번호를 다시 한번 더 입력하세요');
+            $("#inputPasswordCheck").focus();
+            return false;
+        }
+        
+        if($("#input_Password").val()!== $("#input_PasswordCheck").val()){
+            alert('비밀번호를 둘다 동일하게 입력하세요');
+            return false;
+        }
+
+        if($("#input_Mobile").val() ==''){
+            alert('휴대폰 번호를 입력하세요');
+            $("#inputMobile").focus();
+            return false;
+        }
+        
+        if($("#input_address").val() ==''){
+            alert('주소를 입력하세요');
+            $("#input_address").focus();
+            return false;
+        }
+	})
+})
